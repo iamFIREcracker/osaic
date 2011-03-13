455d0dc8bcdb4a8c270bc50412c3a6e62c01032e
Matteo Landi <landimatte@gmail.com>
2011-03-13 12:08:15 +0100

    Change version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = '1.1.1'
+VERSION = '1.2.1'
 NAME = 'osaic'
 MODULES = [NAME]
 DESCRIPTION = 'Create mosaics from images with ``python -mosaic image``'
